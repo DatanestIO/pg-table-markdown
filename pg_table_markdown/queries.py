392bae05620e0f99a7564f4f12cf6f633e229c4e
zhezack <31434672+zhezack@users.noreply.github.com>
2018-07-16 14:19:39 +0700

    Update queries.py

--- pg_table_markdown/queries.py
+++ pg_table_markdown/queries.py
@@ -1,12 +1,17 @@
 def build_schema_query(table_schema):
     schema_query = """
-    SELECT      table_name,
-                column_name,
-                column_default,
-                is_nullable,
-                data_type,
-                character_maximum_length
-    FROM        information_schema.columns
+    SELECT      a.table_name,
+                a.column_name,
+                a.column_default,
+                a.is_nullable,
+                a.data_type,
+                a.character_maximum_length,
+                (
+    		        SELECT pg_catalog.col_description(c.oid, a.ordinal_position::int)
+        	        FROM pg_catalog.pg_class c
+        	        WHERE c.oid     = (SELECT a.table_name::regclass::oid) and c.relname = a.table_name
+    	        ) as desctiption
+    FROM        information_schema.columns a
     WHERE       table_schema = '{0}'
     ORDER BY    table_name,
                 ordinal_position
